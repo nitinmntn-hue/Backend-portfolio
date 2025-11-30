9bfe407da32e3845b6d6efd57a3f614422d95c2a
Nitin <nitinm.ntn@gmail.com>
2025-11-30 18:51:21 +0530

    Add express.urlencoded middleware to server
    Added express.urlencoded middleware to handle URL-encoded data.

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const contactRoutes = require("./routes/contactRoutes");
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
@@ -26,3 +27,4 @@ sequelize.sync({ force: true }).then(() => console.log("Database synced"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
