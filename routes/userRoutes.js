7d6fe0ea6824bc81ffa736f8fed580ab9187aadc
Nitin <nitinm.ntn@gmail.com>
2025-11-30 18:12:47 +0530

    Refactor userRoutes.js for cleaner code structure

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,13 +5,10 @@ const router = express.Router();
 
 // Get user info (public)
 router.get("/:id", userController.getUser);
-
 // Update user info (protected)
 router.put("/:id", auth, userController.updateUser);
-
 // Delete education record (protected)
 router.delete("/:userId/education/:eduId", auth, userController.deleteEducation);
-
 // Delete experience record (protected)
 router.delete("/:userId/experience/:expId", auth, userController.deleteExperience);
 
