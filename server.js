bdda5c44663437a691419c444fbaa67cc424678c
Nitin <nitinm.ntn@gmail.com>
2025-11-30 18:56:03 +0530

    Integrate dotenv and add root route
    Added dotenv for environment variable management and included a root route.

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const sequelize = require("./models/index");
+const dotenv = require("dotenv");
+
+// Load environment variables from .env file
+dotenv.config();
 
 const userRoutes = require("./routes/userRoutes");
 const authRoutes = require('./routes/authRoutes');
@@ -13,7 +17,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
-
+// Example route
+app.get("/", (req, res) => {
+  res.json({ message: "Backend Portfolio API is running 🚀" });
+});
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
@@ -27,4 +34,5 @@ sequelize.sync({ force: true }).then(() => console.log("Database synced"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
